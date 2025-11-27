f3bd8981b06f51ff2b3523a7ca9c7290e447dd44
emanueleumberto <info@emanueleumberto.it>
2025-11-27 18:02:09 +0100

    Soluzione DDL Esercizio SQL

--- Esercizio_SQL.sql
+++ Esercizio_SQL.sql
@@ -49,8 +49,122 @@ Compiti da svolgere:
     10. Eliminare la colonna "birth_year" dalla tabella Authors
     11. Eliminare la tabella Authors.
     12. Eliminare il database LibraryDB.
+
+*/    
     
-    
+-- 1. Creare il database chiamato LibraryDB.
+CREATE DATABASE IF NOT EXISTS library_db;
+   
+-- 2. Selezionare il database per l'uso.   
+USE library_db;
+
+-- 3. Creare la tabella Authors con i seguenti campi:
+--        author_id (INT, chiave primaria, autoincrementata)
+--        name (VARCHAR(100), non nullo)
+--        birth_year (INT, può essere nullo)
+
+CREATE TABLE IF NOT EXISTS library_db.authors (
+	author_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    birth_year INT NULL,
+    CONSTRAINT PRIMARY KEY(author_id)
+);
+
+-- 4. Creiamo le seguenti relazioni
+-- One-to-One (1:1) → Relazione tra Utenti e Dettagli Utente
+-- Ogni utente ha un solo profilo con dettagli aggiuntivi.
+
+-- Creare la tabella Users con i seguenti campi:
+-- 		user_id (INT, chiave primaria, autoincrementata)
+--      name (VARCHAR(255))
+--      email (VARCHAR(255))
+CREATE TABLE IF NOT EXISTS library_db.users (
+	user_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    CONSTRAINT PRIMARY KEY(user_id)
+);
+
+-- Creiamo una tabella UserDetails con chiave primaria condivisa.
+--      user_id (INT, chiave primaria, autoincrementata)
+--      address (VARCHAR(255))
+--      phone_number (VARCHAR(15))
+CREATE TABLE IF NOT EXISTS library_db.userdetails (
+	user_id INT NOT NULL AUTO_INCREMENT,
+    address VARCHAR(255) NULL,
+    phone_number VARCHAR(15) NOT NULL UNIQUE,
+    CONSTRAINT PRIMARY KEY(user_id),
+    CONSTRAINT FOREIGN KEY(user_id) REFERENCES users(user_id)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- One-to-Many (1:N) → Relazione tra Autori e Libri
+-- Un autore può scrivere più libri, ma un libro ha un solo autore.
+
+-- Creare la tabella Books con i seguenti campi:
+-- 		book_id (INT, chiave primaria, autoincrementata)
+--      title (VARCHAR(255), non nullo)
+--      publication_year (INT, può essere nullo)
+--      author_id (INT, chiave esterna che fa riferimento a Authors)
+ CREATE TABLE IF NOT EXISTS library_db.books (
+	book_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    publication_year INT NOT NULL,
+    author_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY(book_id),
+    CONSTRAINT FOREIGN KEY(author_id) REFERENCES authors(author_id)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+ 
+-- Many-to-Many (M:N) → Relazione tra Libri e Utenti (Prestiti)
+-- Un utente può prendere in prestito più libri, e un libro può essere preso in prestito da più utenti.
+            
+-- Per gestire questa relazione, creiamo una tabella di collegamento (Loans).
+-- 		loan_id (INT, chiave primaria, autoincrementata)
+--      user_id (INT, non nullo)
+--      book_id (INT, non nullo)
+--      loan_date (DATE, non nullo)
+--      return_date (DATE, può essere nullo)
+ CREATE TABLE IF NOT EXISTS library_db.loans (
+	loan_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    loan_date DATE NOT NULL,
+    return_date DATE NULL,
+    CONSTRAINT PRIMARY KEY(loan_id),
+    CONSTRAINT FOREIGN KEY(user_id) REFERENCES users(user_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FOREIGN KEY(book_id) REFERENCES books(book_id)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+ 
+-- 5. Modificare la tabella Books aggiungendo una colonna genre (VARCHAR(50)).
+ALTER TABLE library_db.books ADD COLUMN genre VARCHAR(50) NULL;
+
+-- 6. Aggiungere una colonna "ISBN" alla tabella Books
+ALTER TABLE library_db.books ADD COLUMN isbn CHAR(13) NOT NULL UNIQUE;
+
+-- 7. Modificare il tipo di dato della colonna birth_year in Authors per accettare solo date  di tipo YEAR.
+ALTER TABLE library_db.authors MODIFY COLUMN birth_year YEAR;  
+
+-- 8. Modificare il tipo di dato di "phone_number" in UserDetails in CHAR
+ALTER TABLE library_db.userdetails MODIFY COLUMN phone_number CHAR(10);  
+
+-- 9. Eliminare la colonna genre dalla tabella Books.
+ALTER TABLE library_db.books DROP COLUMN genre;
+
+-- 10. Eliminare la colonna "birth_year" dalla tabella Authors
+ALTER TABLE library_db.authors DROP COLUMN birth_year;
+
+-- 11. Eliminare la tabella Authors.
+ALTER TABLE library_db.books DROP CONSTRAINT books_ibfk_1;
+ALTER TABLE library_db.books DROP COLUMN author_id;
+DROP TABLE library_db.authors;
+
+-- 12. Eliminare il database LibraryDB.
+DROP DATABASE library_db;
+  
+ /*   
     -- Esercizio DML 
 -- Inserire dai 5 ai 10 valori corretti in ogni tabella del database librarydb 
 	testando le relazioni e i vincoli di ogni tabella
@@ -63,4 +177,15 @@ Compiti da svolgere:
 -- Aggiornare indirizzo di un utente
 -- Aggiornare le informazioni di un libro
 -- Aggiornare le informazioni sulla tabella prestiti
+
+-- Esercizio DQL 
+-- Recuperare Dati con Query (SELECT)
+-- 1. Visualizzare tutti gli utenti e i loro dettagli
+-- 2. Mostrare tutti i libri e i rispettivi autori 
+-- 3. Recuperare tutti i prestiti con nomi degli utenti e titoli dei libri
+-- 4. Trovare tutti i libri non ancora restituiti
+-- 5. Contare quanti libri ha scritto ogni autore
+    
+    
+    
     
